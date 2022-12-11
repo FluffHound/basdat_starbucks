e5fc87478095d55689a3bfd137803f2d7b65a3f0
Muhammad Hanif Sudibyo <mhsudibyo@gmail.com>
2022-12-11 11:34:58 +0700

    Add .env

---
+++ web_DW_laravel/resources/views/testsite.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <title>Starbucks OLAP Panel</title>
+        <link rel="icon" type="image/x-icon" href="./starbucks_resource/assets/favicon.ico" />
+        <!-- Font Awesome icons (free version)-->
+        <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
+        <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Tinos:ital,wght@0,400;0,700;1,400;1,700&amp;display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,wght@0,400;0,500;0,700;1,400;1,500;1,700&amp;display=swap" rel="stylesheet" />
+        <!-- Core theme CSS & JS(includes Bootstrap)-->
+        <link href="./css/styles.css" rel="stylesheet"/>
+        <script src="https://code.jquery.com/jquery-3.6.1.js" crossorigin="anonymous"></script>
+    </head>
+    <body class="sb-nav-fixed">
+        <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
+
+            <!-- Navbar Brand-->
+            <img class="img-fluid" src="./assets/img/Starbucks_Logo-removebg-preview.png" alt="logo" style="width: 45px;" />
+            <a class="navbar-brand ps-3" href="index.html">TESTSITE</a>
+    
+            <!-- Sidebar Toggle-->
+            <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
+            <!-- Navbar-->
+        </nav>
+    </body>
+</html>

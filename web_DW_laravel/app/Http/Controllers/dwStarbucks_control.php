59cddfd76c1a5d3b21d2e9519e18f3887d230b97
Muhammad Hanif Sudibyo <mhsudibyo@gmail.com>
2022-12-14 08:17:08 +0700

    Fix controller

--- web_DW_laravel/app/Http/Controllers/dwStarbucks_control.php
+++ web_DW_laravel/app/Http/Controllers/dwStarbucks_control.php
@@ -24,8 +24,6 @@ class dwStarbucks_control extends Controller
     {
         // DIM cabang - jumlah cabang per provinsi
         $dbCabang = DB::table('dim_cabang')
-<<<<<<< Updated upstream
-=======
                     ->select(DB::raw('id_provinsi, nama_provinsi, COUNT(id_toko) AS jumlahToko'))
                     ->groupBy('nama_provinsi', 'id_provinsi')
                     ->orderBy('id_provinsi')
@@ -72,7 +70,6 @@ class dwStarbucks_control extends Controller
     public function grafik()
     {
         $datas = DB::table('dim_cabang')
->>>>>>> Stashed changes
                     ->select(DB::raw('id_provinsi, nama_provinsi, COUNT(id_toko) AS jumlahToko'))
                     ->groupBy('nama_provinsi', 'id_provinsi')
                     ->orderBy('id_provinsi')
@@ -111,13 +108,6 @@ class dwStarbucks_control extends Controller
         $labelTipeProduk = $dbTipeProduk -> pluck('id_tipe_produk');
         $dataTipeProduk = $dbTipeProduk -> pluck('jumlahTipeProduk');
         
-<<<<<<< Updated upstream
-        return view('testsite', compact('dbCabang', 'labelCabang', 'dataCabang',
-                                        'dbKaryawan', 'labelKaryawan', 'dataKaryawan',
-                                        'dbProdukPerTipe', 'labelProdukPerTipe', 'dataProdukPerTipe',
-                                        'dbTipeProduk', 'labelTipeProduk', 'dataTipeProduk'));
-=======
         return view('landing', compact('datas', 'labels', 'data'));
->>>>>>> Stashed changes
     }
 }
